f573121f13e6c917495323cd1c12145bef3e9db5
钟孟捷 <718335940@qq.com>
2023-04-19 02:06:24 +0800

    test

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -4,6 +4,7 @@ exports.handler = async function (event, context) {
   return {
     statusCode: 200,
     body: await PythonShell.run('subcvt-mannix.py', {
+      pythonPath: 'python',
       args: [event.rawUrl]
     })
   }
