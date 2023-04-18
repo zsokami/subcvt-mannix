e7854cdc777d5ea5e32c0b77c3b51aac198607d1
钟孟捷 <718335940@qq.com>
2023-04-19 03:03:14 +0800

    test

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,8 +1,11 @@
+const { URL } = require('url')
 const axios = require('axios')
 
 exports.handler = async function (event, context) {
+  const url = new URL(event.rawUrl)
+  url.host = 'api.dler.io'
   return {
     statusCode: 200,
-    body: `;${event.rawUrl}\n${(await axios.get('https://dd.al/config')).data}`
+    body: `;${event.rawUrl}\n${(await axios.get(url)).data}`
   }
 }
