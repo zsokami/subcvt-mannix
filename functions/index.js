7a5d34c2afea329b235676254a19b14bf0dc647b
钟孟捷 <718335940@qq.com>
2023-04-19 20:08:41 +0800

    test

--- functions/index.js
+++ functions/index.js
@@ -6,8 +6,8 @@ exports.handler = async function ({ rawUrl }) {
   const url = new URL(rawUrl)
   url.host = 'api.dler.io'
   const { data } = await axios.get(url)
-  const y = YAML.load(data)
-  const y_str = YAML.dump(y)
+  const y = YAML.load(data, {json: true, strict: false})
+  const y_str = YAML.dump(y, {flowLevel: 1})
   return {
     statusCode: 200,
     body: `${y}\n\n${y_str}`
