6d40425fd6da07e531f937b595ebb7862afd0649
钟孟捷 <718335940@qq.com>
2023-04-19 22:07:05 +0800

    V1 订阅转换 api 反代

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,22 @@
 const { URL } = require('url')
 const axios = require('axios')
-const YAML = require('js-yaml')
 
 exports.handler = async function ({ rawUrl }) {
   const url = new URL(rawUrl)
-  url.host = 'api.dler.io'
+  const path = url.pathname.split('/')
+  if (path[1].includes('.')) {
+    url.host = path.splice(1, 1)[0]
+    url.pathname = path.join('/')
+  } else {
+    url.host = 'api.dler.io'
+  }
+  if (!path[1]) {
+    url.pathname = 'sub'
+  }
+  
   const { data } = await axios.get(url)
-  const y = YAML.load(data, {json: true, strict: false})
-  const y_str = YAML.dump(y, {flowLevel: 1})
   return {
     statusCode: 200,
-    body: `${y}\n\n${y_str}`
+    body: data
   }
 }
