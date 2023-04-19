95019689182bdb32bed85a1172eeb81afc8f9312
钟孟捷 <718335940@qq.com>
2023-04-19 14:54:19 +0800

    test

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,15 @@
 const { URL } = require('url')
 const axios = require('axios')
+const YAML = require('yaml')
 
-exports.handler = async function (event, context) {
-  const url = new URL(event.rawUrl)
+exports.handler = async function ({ rawUrl }) {
+  const url = new URL(rawUrl)
   url.host = 'api.dler.io'
+  const { data } = await axios.get(url)
+  const y = YAML.parse(data)
+  const y_str = YAML.stringify(y)
   return {
     statusCode: 200,
-    body: `;${event.rawUrl}\n${(await axios.get(url)).data}`
+    body: `${y}\n\n${y_str}`
   }
 }
