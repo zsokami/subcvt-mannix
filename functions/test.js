a720b18958b12208cf46d0c6bebd6d39af3c6123
钟孟捷 <718335940@qq.com>
2023-04-21 03:28:45 +0800

    test

--- functions/test.js
+++ functions/test.js
@@ -3,6 +3,6 @@ const axios = require('axios')
 exports.handler = async () => {
   return {
     statusCode: 200,
-    body: await axios.get('http://127.0.0.1/test.txt').then(r => r.data)
+    body: await axios.get('https://127.0.0.1/test.txt').then(r => r.data)
   }
 }
