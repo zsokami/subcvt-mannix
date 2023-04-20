b071d7ce09d7ee3a0517c17f31982d5ef9d44b7b
钟孟捷 <718335940@qq.com>
2023-04-21 03:01:46 +0800

    test

---
+++ functions/test.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const fs = require('fs').promises
+
+
+
+exports.handler = async () => {
+  const body = {
+    __dirname,
+    'ls __dirname': await fs.readdir(__dirname).catch(e => e),
+    'cwd': path.resolve(),
+    'ls cwd': await fs.readdir('.').catch(e => e),
+    '__dirname/..': path.dirname(__dirname),
+    'ls __dirname/..': await fs.readdir(path.dirname(__dirname)).catch(e => e)
+  }
+
+  return {
+    statusCode: 200,
+    body
+  }
+}
