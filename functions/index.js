41772fabb7396fbca2b7d36946815e6fc7742545
钟孟捷 <718335940@qq.com>
2023-04-20 14:55:47 +0800

    test

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const { URL } = require('url')
 const axios = require('axios')
+const YAML = require('yaml')
 
 exports.handler = async function ({ rawUrl }) {
   const url = new URL(rawUrl)
@@ -13,10 +14,22 @@ exports.handler = async function ({ rawUrl }) {
   if (!path[1]) {
     url.pathname = 'sub'
   }
-  
-  const { data } = await axios.get(url)
+  let { data } = await axios.get(url)
+  if (
+    url.pathname == '/sub' &&
+    url.searchParams.get('target') == 'clash' &&
+    url.searchParams.get('list') != 'true' &&
+    url.searchParams.get('expand') != 'false'
+  ) {
+    data = remove_redundant_groups(data)
+  }
   return {
     statusCode: 200,
     body: data
   }
+}
+
+function remove_redundant_groups (clash) {
+  const doc = YAML.parseDocument(clash, { version: '1.1' })
+  return doc.toString({ lineWidth: 0 })
 }
