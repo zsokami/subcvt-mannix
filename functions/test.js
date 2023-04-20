af8a200888b018d371bb3da3b7c70426ee003c62
钟孟捷 <718335940@qq.com>
2023-04-21 03:27:02 +0800

    test

--- functions/test.js
+++ functions/test.js
@@ -1,21 +1,8 @@
-const path = require('path')
-const fs = require('fs').promises
-
-
+const axios = require('axios')
 
 exports.handler = async () => {
-  const body = {
-    __dirname,
-    'ls __dirname': await fs.readdir(__dirname).catch(e => e),
-    'cwd': path.resolve(),
-    'ls cwd': await fs.readdir('.').catch(e => e),
-    '__dirname/..': path.dirname(__dirname),
-    'ls __dirname/..': await fs.readdir(path.dirname(__dirname)).catch(e => e),
-    'ls /': await fs.readdir('/').catch(e => e)
-  }
-
   return {
     statusCode: 200,
-    body: JSON.stringify(body)
+    body: await axios.get('http://127.0.0.1/test.txt').then(r => r.data)
   }
 }
